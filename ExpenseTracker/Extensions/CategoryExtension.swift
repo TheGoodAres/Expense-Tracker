ea3392444dbf4b4577dcf46a3d04b3d0475a2269
TheGoodAres <61661335+TheGoodAres@users.noreply.github.com>
2023-12-06 16:26:15 +0000

    Added additional views to reduce code repetition

---
+++ ExpenseTracker/Extensions/CategoryExtension.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryExtension.swift
+//  ExpenseTracker
+//
+//  Created by Robert-Dumitru Oprea on 05/12/2023.
+//
+
+import Foundation
---
+++ ExpenseTracker/Views/SmallTransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  SmallTransactionView.swift
+//  ExpenseTracker
+//
+//  Created by Robert-Dumitru Oprea on 06/12/2023.
+//
+
+import SwiftUI
+
+struct SmallTransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallTransactionView()
+}
